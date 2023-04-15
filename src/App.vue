ec1c8d53f4b0b060746895e1891cac3faf661036
Florexianer <florian.kozel@gmail.com>
2023-04-15 17:47:19 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <svg width="600" height="100">
+    <g class="chart" transform="translate(50, 0)">
+    </g>
+  </svg>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as d3 from "d3";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  mounted() {
+    const myData = [40, 10, 20, 60, 30];
+
+    d3.select('.chart')
+          .selectAll('circle')
+        .data(myData)
+        .join('circle')
+        .attr('cx', function(d, i) {
+          return i * 100;
+        })
+        .attr('cy', 60)
+        .attr('r', function(d) {
+          return 0.5 * d;
+        })
+        .style('fill', 'orange');
+
   }
 }
 </script>
